641bda7cc9a4d7493ef7a4dd1756a213d8e5ba99
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-02-06 20:59:43 +0500

    Code Rub

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string firstInput = Console.ReadLine();
+string secondInput = Console.ReadLine();
+
+int firstNumber = Convert.ToInt32(firstInput);
+int secondNumber = Convert.ToInt32(secondInput);
+
+Console.WriteLine(firstNumber + secondNumber);
